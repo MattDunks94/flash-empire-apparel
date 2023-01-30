4580585776b1da74efe3685ebb7dcdf5948a6942
Matt <mjd014@live.co.uk>
2023-01-30 10:14:11 +0000

    modified settings deployment not working, application error.

--- flash_empire_apparel/settings.py
+++ flash_empire_apparel/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['flash-empire-apparel.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['flash-empire-apparel.herokuapp.com', 'localhost']
 
 
 # Application definition
