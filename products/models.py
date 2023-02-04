2de4253d2336d47a52d44dc7fa4adb9ba0fd3ab9
Matt <mjd014@live.co.uk>
2023-02-04 16:18:04 +0000

    added a verbose name plural for categories model, removed image url field from product model.

--- products/models.py
+++ products/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=200)
     friendly_name = models.CharField(max_length=200, null=True, blank=True)
 
@@ -25,7 +28,6 @@ class Product(models.Model):
     price = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True
         )
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
