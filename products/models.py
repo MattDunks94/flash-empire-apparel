6f4b07fc52db3f67c9cb1f7aeacf033163693b4f
Matt <mjd014@live.co.uk>
2023-02-04 16:02:06 +0000

    added products and categorys model, registered them to the admin.

---
+++ products/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    friendly_name = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'Category', on_delete=models.SET_NULL, null=True, blank=True
+        )
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    sku = models.CharField(max_length=200, null=True, blank=True)
+    has_size = models.BooleanField(default=False, null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True
+        )
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
