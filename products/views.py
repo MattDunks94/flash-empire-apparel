f5690023775d7051d259b61728a471eb8847be78
Matt <mjd014@live.co.uk>
2023-02-13 18:58:17 +0000

    added search product functionality, search via name or description.

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -8,9 +10,25 @@ def all_products(request):
     """ A view for viewing all products, including sorting and search queries """
 
     products = Product.objects.all()
+    search_query = None
+
+    # Search bar query
+    if request.GET:
+        if 'q' in request.GET:
+            search_query = request.GET['q']
+            if not search_query:
+                messages.error(request, "The empire has declined your query,\
+                nothing was entered. Please try again.")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=search_query) | Q(
+                description__icontains=search_query
+                )
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': search_query,
     }
     return render(request, 'products/products.html', context)
 
