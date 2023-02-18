136afb8d29c1e3c5340605069470a7e1c15244e0
Matt <mjd014@live.co.uk>
2023-02-18 17:41:20 +0000

    updated contexts.py in bag app, calculated grand total of products added to bag.

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
+from django.shortcuts import get_object_or_404
 from decimal import Decimal
 from django.conf import settings
+from products.models import Product
 
 
 def bag_contents(request):
@@ -7,6 +9,17 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,            
+        })
 
     # if total is less than free delivery threshold, calculate delivery charge.
     if total < settings.FREE_DELIVERY_THRESHOLD:
@@ -14,7 +27,7 @@ def bag_contents(request):
         # The remaining amount the customer needs to spend for free delivery.
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
-        total = 0
+        delivery = 0
         free_delivery_delta = 0
     # Total cost including delivery.
     grand_total = delivery + total
