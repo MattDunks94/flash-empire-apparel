4dd1ff3768e4aadd1b09c712eeb37604a0cb4447
Matt <mjd014@live.co.uk>
2023-02-20 12:39:09 +0000

    added remove from bag view, url, remove item functionality, also created custom templatetag for subtotal calculation.

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -41,7 +41,7 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """ Adjust specified product in shopping bag """
 
-    quantity = int(request.POST.get('quantity'))
+    quantity = 1
     bag = request.session.get('bag', {})
     size = None
     if 'product_size' in request.POST:
@@ -52,12 +52,37 @@ def adjust_bag(request, item_id):
             bag[item_id]['items_by_size'][size] = quantity
         else:
             del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
     else:
         if quantity > 0:
             bag[item_id] = quantity
         else:
-            bag.pop[item_id]
+            bag.pop(item_id)
 
     request.session['bag'] = bag
 
     return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ Remove specified product in shopping bag """
+    try:
+        bag = request.session.get('bag', {})
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
