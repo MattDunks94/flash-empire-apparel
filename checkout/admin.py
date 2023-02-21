12adb5d8cf00ef64d96e5ca98985aaf855b07bf9
Matt <mjd014@live.co.uk>
2023-02-21 16:43:05 +0000

    added order line item, order  admin, created signals file, imported it in apps.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
 # Register your models here.
+
+
+class OrderLineItemAdmin(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = (
+        'lineitem_total',
+    )
+
+
+class OrderAdmin(admin.ModelAdmin):
+
+    inlines = (OrderLineItemAdmin,)
+    readonly_fields = (
+        'order_number',
+        'date',
+        'delivery_cost',
+        'grand_total',
+        'order_total',
+    )
+
+    fields = (
+        'order_number',
+        'date',
+        'delivery_cost',
+        'grand_total',
+        'order_total',
+        'full_name',
+        'email',
+        'phone_number',
+        'postcode',
+        'town_or_city',
+        'street_address_1',
+        'street_address_2',
+        'county',
+        'country',
+    )
+
+    list_display = (
+        'order_number',
+        'date',
+        'full_name',
+        'delivery_cost',
+        'grand_total',
+        'order_total',
+    )
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
