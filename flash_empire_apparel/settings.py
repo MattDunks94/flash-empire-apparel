0cd7ace97c5ada37ae6c175c4363ce9749fbf1b1
Matt <mjd014@live.co.uk>
2023-02-23 16:51:05 +0000

    cleaned up code

--- flash_empire_apparel/settings.py
+++ flash_empire_apparel/settings.py
@@ -182,8 +182,6 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
 STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
