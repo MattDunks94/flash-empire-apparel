5fed655c2880c5d46c5f1ce628dd263277057677
Matt <mjd014@live.co.uk>
2023-02-24 18:55:54 +0000

    added class methods to webhook handler and webhook view.

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -11,6 +11,22 @@ class StripeWH_Handler:
         """ Handles unexpected, unknown webhook event  """
 
         return HttpResponse(
-            content=f"Webhook received: {event['type']}"
+            content=f'Webhook received: {event["type"]}',
+            status=200,
+        )
 
+    def handle_payment_intent_succeeded(self, event):
+        """ Handles payment_intent.succeeded webhook  """
+
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200,
+        )
+
+    def handle_payment_intent_payment_failed(self, event):
+        """ Handles payment_intent.payment_failed webhook  """
+
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200,
         )
