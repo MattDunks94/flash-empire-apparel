38d69bce7bbbc6c22b0817005d20b50e6fc33e17
Matt <mjd014@live.co.uk>
2023-02-25 16:04:43 +0000

    created user profile form for updating profile, added input styling, updated profile view to display form.

---
+++ profiles/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('default_user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_email': 'Email Address',
+            'default_phone_number': 'Phone Number',
+            'default_postcode': 'Postcode',
+            'default_country': 'Country',
+            'default_town_or_city': 'Town / City',
+            'default_street_address_1': 'Street Address 1',
+            'default_street_address_2': 'Street Address 2',
+            'default_county': 'County',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black profile-form-input'
+            self.fields[field].label = False
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 # Create your views here.
 
@@ -10,9 +12,20 @@ def profile(request):
 
     profile = get_object_or_404(UserProfile, default_user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Updated Profile')
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profile/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'order': orders,
+        'on_profile_page': True,
     }
 
     return render(request, template, context)
