090c9dae0a5a7b740fae82dfbbcfcbf15d263868
Matt <mjd014@live.co.uk>
2023-03-18 14:57:55 +0000

    created view for viewing all blog posts.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+
+def view_post_list(request):
+    """ A view for all blog posts """
+
+    posts = Post.objects.filter(status=1).order_by('-created_on')
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'blog/blog.html', context)
