6361f73330b17ea08b18f31c756162b838d7e28d
Matt <mjd014@live.co.uk>
2023-03-19 16:39:57 +0000

    added add blog post view for adding new blog post.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Post
+from .forms import BlogPostForm
 
 # Create your views here.
 
@@ -24,3 +27,31 @@ def view_post_detail(request, slug):
     }
 
     return render(request, 'blog/blog-post-detail.html', context)
+
+
+@login_required
+def add_blog_post(request):
+    """ A view for adding new blog posts """
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Only the gods of the empire have permission!')
+        return redirect(reverse('home'))
+    
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            messages.success(request, 'Successfully added blog post!')
+            return redirect(reverse('post_detail', args=[post.slug]))
+        else:
+            messages.error(request, 'Unable to add blog post! Please ensure \
+            the form is valid.')
+    else:
+        form = BlogPostForm()
+
+    template = 'blog/add-blog-post.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
