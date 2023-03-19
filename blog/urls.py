7b6f6b39019b5b8505d8cb54fea165e80411f1ad
Matt <mjd014@live.co.uk>
2023-03-19 21:14:54 +0000

    modified the order of urls, was receiving page not found error with add_blog_post view, solved it by reordering urls..

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.view_post_list, name='view_post_list'),
-    path('<slug:slug>/', views.view_post_detail, name='post_detail'),
     path('add_blog_post/', views.add_blog_post, name='add_blog_post'),
+    path('<slug:slug>/', views.view_post_detail, name='post_detail'),
 ]
