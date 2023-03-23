6176166e98bf78fe4ad19a3f25246aca0ea7fb82
Matt <mjd014@live.co.uk>
2023-03-23 13:09:28 +0000

    created view for adding products to wishlist.

--- wishlist/views.py
+++ wishlist/views.py
@@ -12,6 +12,7 @@ from profiles.models import UserProfile
 @login_required
 def view_wishlist(request):
     """ A view for viewing user's wishlist """
+
     if not request.user.is_authenticated:
         messages.error(request, 'You must be pledged to the empire to be \
         granted a wish. Please login or sign up.')
@@ -25,3 +26,35 @@ def view_wishlist(request):
     }
 
     return render(request, 'wishlist/wishlist.html', context)
+
+
+@login_required
+def add_to_wishlist(request, product_id):
+    """ A view to add product to user's wishlist """
+
+    if not request.user.is_authenticated:
+        messages.error(request, 'You must be pledged to the empire to be \
+        granted a wish. Please login or sign up.')
+        return redirect(reverse('account_login'))
+
+    user = get_object_or_404(UserProfile, default_user=request.user)
+    product = get_object_or_404(Product, pk=product_id)
+
+    # Checks whether added wished_item is already in user's wishlist.
+    existing_product = Wishlist.objects.filter(
+        wished_product=product,
+        user_profile=user
+        ).exists()
+
+    if existing_product:
+        messages.info(request, f'{product.name} has already been wished for!')
+        return redirect(reverse('product_detail', args=[product_id]))
+        # Adds wished item to wishlist.
+    else:
+        wished_item = Wishlist.objects.create(
+            user_profile=user,
+            wished_product=product
+            )
+        messages.success(request, f'Your wish for {product.name} has been \
+        added to your wishlist!')
+        return redirect(reverse('product_detail', args=[product_id]))
