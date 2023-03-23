f1643102ea64f97583895f0b863f454e81da1f24
Matt <mjd014@live.co.uk>
2023-03-23 10:50:32 +0000

    created wishlist model in wishlist app, made migrations.

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from profiles.models import UserProfile
+from products.models import Product
 
 # Create your models here.
+
+
+class Wishlist(models.Model):
+
+    user_profile = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE, null=True, blank=True
+        )
+    wished_product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, null=True, blank=True, default=1
+        )
+    date_added = models.DateTimeField(
+        auto_now_add=True, blank=False, null=False
+        )
+
+    def __str__(self):
+        return f'Wishlist ({self.user_profile})'
