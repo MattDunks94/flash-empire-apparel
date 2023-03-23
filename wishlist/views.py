107f3a537f8eafa19b824591fe8815057308b6b5
Matt <mjd014@live.co.uk>
2023-03-23 11:07:46 +0000

    created view for viewing users wishlist.

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+from .models import Wishlist
+from products.models import Product
+from profiles.models import UserProfile
 
 # Create your views here.
+
+
+@login_required
+def view_wishlist(request):
+    """ A view for viewing user's wishlist """
+    if not request.user.is_authenticated:
+        messages.error(request, 'You must be pledged to the empire to be \
+        granted a wish. Please login or sign up.')
+        return redirect(reverse('account_login'))
+
+    user = get_object_or_404(UserProfile, user=request.user)
+    wishlist = Wishlist.objects.filter(user_profile=user)
+
+    context = {
+        'wishlist': wishlist
+    }
+
+    return render(request, 'wishlist/wishlist.html', context)
