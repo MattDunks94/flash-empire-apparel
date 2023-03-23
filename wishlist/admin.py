71aa7480852783c901f22895c5ea1d2aaaacb0b1
Matt <mjd014@live.co.uk>
2023-03-23 11:09:38 +0000

    registered wishlist model to admin.

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Wishlist
 
 # Register your models here.
+
+
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = (
+        'user_profile',
+        'wished_product',
+        'date_added',
+    )
+
+    ordering = ('-date_added',)
+
+
+admin.site.register(Wishlist, WishlistAdmin)
