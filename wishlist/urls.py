81a4a49b3ebbe41ad56a1d2e6e0400141b8b1fa0
Matt <mjd014@live.co.uk>
2023-03-23 13:26:18 +0000

    added 'add to wishlist' view to urls.

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.view_wishlist, name='wishlist'),
+    path(
+        'add_to_wishlist/<int:product_id>', views.add_to_wishlist, name='add_to_wishlist'
+        )
 ]
